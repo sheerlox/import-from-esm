16d85b43894776001d5d2d7d3577d9e1f89d15cc
Pierre Cavin <me@sherlox.io>
2023-10-04 19:34:39 +0200

    test: test both CJS & ESM modules

--- test.js
+++ test.js
@@ -1,18 +1,34 @@
 import test from 'ava';
 import importFrom from './index.js';
 
-test('importFrom()', t => {
-	t.is(importFrom('fixture', './fixture.cjs'), 'unicorn');
-	t.throws(() => importFrom('fixture', './nonexistent'));
+test('importFrom() (CJS)', async t => {
+	t.is(await importFrom('fixture', './fixture-cjs.cjs'), 'unicorn');
+	await t.throwsAsync(importFrom('fixture', './nonexistent'));
 
-	const moduleNotFoundError = t.throws(() => {
-		importFrom('fixture', './nonexistent');
-	});
+	const moduleNotFoundError = await t.throwsAsync(
+		importFrom('fixture', './nonexistent')
+	);
 	t.is(moduleNotFoundError.code, 'MODULE_NOT_FOUND');
 	t.regex(moduleNotFoundError.message, /^Cannot find module '.\/nonexistent'/);
 });
 
-test('importFrom.silent()', t => {
-	t.is(importFrom.silent('fixture', './fixture.cjs'), 'unicorn');
-	t.is(importFrom.silent('fixture', './nonexistent'), undefined);
+test('importFrom() (ESM)', async t => {
+	t.is(await importFrom('fixture', './fixture-esm.js'), 'unicorn');
+	await t.throwsAsync(importFrom('fixture', './nonexistent'));
+
+	const moduleNotFoundError = await t.throwsAsync(
+		importFrom('fixture', './nonexistent')
+	);
+	t.is(moduleNotFoundError.code, 'MODULE_NOT_FOUND');
+	t.regex(moduleNotFoundError.message, /^Cannot find module '.\/nonexistent'/);
+});
+
+test('importFrom.silent() (CJS)', async t => {
+	t.is(await importFrom.silent('fixture', './fixture-cjs.cjs'), 'unicorn');
+	t.is(await importFrom.silent('fixture', './nonexistent'), undefined);
+});
+
+test('importFrom.silent() (ESM)', async t => {
+	t.is(await importFrom.silent('fixture', './fixture-esm.js'), 'unicorn');
+	t.is(await importFrom.silent('fixture', './nonexistent'), undefined);
 });
