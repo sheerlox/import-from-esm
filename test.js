938c6bd961b45dc3729844e45cccf88acbcaf790
Pierre Cavin <me@sherlox.io>
2023-10-06 01:49:41 +0200

    test: improve tests by testing for various extensions

--- test.js
+++ test.js
@@ -1,34 +1,35 @@
 import test from 'ava';
 import importFrom from './index.js';
 
-test('importFrom() (CJS)', async t => {
-	t.is(await importFrom('fixture', './fixture-cjs.cjs'), 'unicorn');
-	await t.throwsAsync(importFrom('fixture', './nonexistent'));
+const testImportFrom = async (t, dir, file, ext) => {
+	const moduleId = `./${file}${ext ? `.${ext}` : ''}`;
+	const nonExistentModuleId = `./nonexistent${ext ? `.${ext}` : ''}`;
+
+	t.is(await importFrom(dir, moduleId), 'unicorn');
 
 	const moduleNotFoundError = await t.throwsAsync(
-		importFrom('fixture', './nonexistent')
+		importFrom(dir, nonExistentModuleId)
 	);
 	t.is(moduleNotFoundError.code, 'MODULE_NOT_FOUND');
-	t.regex(moduleNotFoundError.message, /^Cannot find module '.\/nonexistent'/);
-});
+	t.regex(moduleNotFoundError.message, new RegExp(`^Cannot find module '${nonExistentModuleId}'`));
+}
 
-test('importFrom() (ESM)', async t => {
-	t.is(await importFrom('fixture', './fixture-esm.js'), 'unicorn');
-	await t.throwsAsync(importFrom('fixture', './nonexistent'));
+test('importFrom() (CJS module in CJS folder - no extension)', t => testImportFrom(t, 'fixture/commonjs', 'fixture-cjs', undefined));
+test('importFrom() (CJS module in CJS folder - .js extension)', t => testImportFrom(t, 'fixture/commonjs', 'fixture-cjs', 'js'));
+test('importFrom() (CJS module in ESM folder - no extension)', t => testImportFrom(t, 'fixture/module', 'fixture-cjs', undefined));
+test('importFrom() (CJS module in ESM folder - .cjs extension)', t => testImportFrom(t, 'fixture/module', 'fixture-cjs', 'cjs'));
 
-	const moduleNotFoundError = await t.throwsAsync(
-		importFrom('fixture', './nonexistent')
-	);
-	t.is(moduleNotFoundError.code, 'MODULE_NOT_FOUND');
-	t.regex(moduleNotFoundError.message, /^Cannot find module '.\/nonexistent'/);
-});
+test('importFrom() (ESM module in ESM folder - no extension)', t => testImportFrom(t, 'fixture/module', 'fixture-esm', undefined));
+test('importFrom() (ESM module in ESM folder - .js extension)', t => testImportFrom(t, 'fixture/module', 'fixture-esm', 'js'));
+test('importFrom() (ESM module in CJS folder - no extension)', t => testImportFrom(t, 'fixture/commonjs', 'fixture-esm', undefined));
+test('importFrom() (ESM module in CJS folder - .mjs extension)', t => testImportFrom(t, 'fixture/commonjs', 'fixture-esm', 'mjs'));
 
 test('importFrom.silent() (CJS)', async t => {
-	t.is(await importFrom.silent('fixture', './fixture-cjs.cjs'), 'unicorn');
-	t.is(await importFrom.silent('fixture', './nonexistent'), undefined);
+	t.is(await importFrom.silent('fixture/module', './fixture-cjs.cjs'), 'unicorn');
+	t.is(await importFrom.silent('fixture/module', './nonexistent'), undefined);
 });
 
 test('importFrom.silent() (ESM)', async t => {
-	t.is(await importFrom.silent('fixture', './fixture-esm.js'), 'unicorn');
-	t.is(await importFrom.silent('fixture', './nonexistent'), undefined);
+	t.is(await importFrom.silent('fixture/module', './fixture-esm.js'), 'unicorn');
+	t.is(await importFrom.silent('fixture/module', './nonexistent'), undefined);
 });
