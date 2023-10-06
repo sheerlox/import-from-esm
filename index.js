4128481c2bb33629e4b6a3e101075b71f95a5607
Pierre Cavin <me@sherlox.io>
2023-10-06 02:00:53 +0200

    refactor: replicate CJS module resolution outlines

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
-import { resolve } from 'path';
+import { builtinModules } from "node:module";
+import { resolve } from "node:path";
+import { loadNpmModule } from "./lib/load-npm-module.js";
 
-async function importFrom (fromDirectory, moduleId) {
-  const localModulePath = resolve(fromDirectory, moduleId);
+async function tryImport(moduleId) {
+	try {
+		return (await import(moduleId)).default;
+	} catch {}
+}
 
-  try {
-    return (await import(moduleId)).default;
-  } catch (e) {
-    try {
-      return (await import(localModulePath)).default;
-    } catch (e) {
-      const error = new Error(`Cannot find module '${moduleId}'`);
-      error.code = "MODULE_NOT_FOUND";
-      throw error;
-    }
-  }
-};
+async function importFrom(fromDirectory, moduleId) {
+	// https://nodejs.org/api/modules.html#core-modules
+	// TODO: write test and make this is working as expected
+	if (/^node:/.test(moduleId) || builtinModules.includes(moduleId)) {
+		return (await import(moduleId)).default;
+	}
+
+	let loadedModule;
+
+	// https://nodejs.org/api/modules.html#file-modules
+	// if moduleId begins with '/', '../', or './'
+	if (/^(\/|\.\.\/|\.\/)/.test(moduleId)) {
+		const localModulePath = resolve(fromDirectory, moduleId);
+
+		// try to resolve exact file path
+		loadedModule = await tryImport(localModulePath);
+
+		if (!loadedModule) {
+			// try to resolve file path with added extensions
+			const extensions = [".js", ".mjs", ".cjs"];
+
+			for (let ext of extensions) {
+				loadedModule = await tryImport(`${localModulePath}${ext}`);
+				if (loadedModule) break;
+			}
+		}
+
+		if (loadedModule) return loadedModule;
+	}
+
+	// https://nodejs.org/api/modules.html#loading-from-node_modules-folders
+	// try to resolve npm module
+	loadedModule = await loadNpmModule(fromDirectory, moduleId);
+	if (loadedModule) return loadedModule;
+
+	const error = new Error(`Cannot find module '${moduleId}'`);
+	error.code = "MODULE_NOT_FOUND";
+	throw error;
+}
 
 importFrom.silent = async function (fromDirectory, moduleId) {
 	try {
-		return (await importFrom(fromDirectory, moduleId));
+		return await importFrom(fromDirectory, moduleId);
 	} catch {}
-}
+};
 
 export default importFrom;
