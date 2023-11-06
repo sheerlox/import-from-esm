c9c917cc5fe6916f2e7bc6dcfc7e8dfc1c44c970
Pierre Cavin <me@sherlox.io>
2023-11-06 17:37:43 +0100

    test: add loading from parent node_modules test

---
+++ tests/test-helpers.js
@@ -0,0 +1,27 @@
+/* eslint-disable max-params */
+
+export const testImportFromLocal = async (t, importFrom, dir, file, ext) => {
+	const extString = ext ? `.${ext}` : '';
+	const moduleId = `./${file}${extString}`;
+	const nonExistentModuleId = `./nonexistent${extString}`;
+
+	t.is(await importFrom.silent(dir, moduleId), 'unicorn');
+
+	const moduleNotFoundError = await t.throwsAsync(importFrom(dir, nonExistentModuleId));
+	t.is(moduleNotFoundError.code, 'MODULE_NOT_FOUND');
+	t.regex(moduleNotFoundError.message, new RegExp(`^Cannot find module '${nonExistentModuleId}'`));
+
+	t.is(await importFrom.silent(dir, nonExistentModuleId), undefined);
+};
+
+export const testImportFromPackage = async (t, importFrom, dir, packageName, expected) => {
+	const nonExistentPackageName = 'nonexistent-package';
+
+	t.is(await importFrom.silent(dir, packageName), expected);
+
+	const moduleNotFoundError = await t.throwsAsync(importFrom(dir, nonExistentPackageName));
+	t.is(moduleNotFoundError.code, 'MODULE_NOT_FOUND');
+	t.regex(moduleNotFoundError.message, new RegExp(`^Cannot find module '${nonExistentPackageName}'`));
+
+	t.is(await importFrom.silent(dir, nonExistentPackageName), undefined);
+};
